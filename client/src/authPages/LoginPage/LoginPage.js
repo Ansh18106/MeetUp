ca21e2a6638528e3d19af99eb289632ead883dd2
Ansh <anshbansal18106@gmail.com>
2023-05-17 23:49:21 +0530

    login and register frontend is ready

--- client/src/authPages/LoginPage/LoginPage.js
+++ client/src/authPages/LoginPage/LoginPage.js
@@ -1,11 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AuthBox from '../../shared/components/AuthBox';
 import LoginPageHeader from './LoginPageHeader';
 import LoginPageInputs from './LoginPageInputs';
+import LoginPageFooter from './LoginPageFooter';
+import { validateLoginForm } from '../../shared/utils/validation';
 
 const LoginPage = () => {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+  
+  const handleLogin = () => {
+    console.log("log in");
+  }
+
   return (
     <AuthBox>
       <LoginPageHeader/>
@@ -14,6 +26,10 @@ const LoginPage = () => {
         setMail={setMail}
         password={password}
         setPassword={setPassword}
+      />
+      <LoginPageFooter
+        isFormValid={isFormValid}
+        handleLogin={handleLogin}
       />
     </AuthBox>
   )
